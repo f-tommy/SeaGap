0e57320310463b95bb7861964618ab56edce4cc1
Fumiaki Tomita <39943988+f-tommy@users.noreply.github.com>
2023-02-19 00:59:33 +0900

    Mod1

--- src/plot_gradmap.jl
+++ src/plot_gradmap.jl
@@ -10,7 +10,7 @@ Make a figure of sea-surface track with color of shallow gradient.
 * `xrange`: Range of EW component in meters when `autoscale=false`
 * `yrange`: Range of NS component in meters when `autoscale=false`
 * `autoscale`: If `autoscale=true`, the plot range is automatically defined
-* `fn1`: File name of the seafloor transponder positions (`fn1="pxp-ini.xyh"` by default)
+* `fn1`: File name of the seafloor transponder positions (`fn1="pxp-ini.ini"` by default)
 * `fn2`: Travel-time residual file name (`fn2="residual_grad.out"` by default)
 * `fno`: Output figure name (`fno="gradmap.pdf"` by default)
 * `show`: if `show=true`, a figure is shown on REPL and is not saved as a file (`show=false` by default)
@@ -29,7 +29,7 @@ Make a figure of sea-surface track with color of shallow gradient.
     plot_gradmap((-2500,2500),(-2500,2500),autoscale=false,fno="gradmap.png")
 
 """
-function plot_gradmap(xrange=(-3000,3000),yrange=(-3000,3000);autoscale=true::Bool,fn1="pxp-ini.xyh"::String,fn2="residual_grad.out"::String,fno="gradmap.pdf"::String,plot_size=(600,500),lmargin=2.5,tmargin=1.0,bmargin=1.0,rmargin=1.0,show=false::Bool,ms1=10::Int64,ms2=3::Int64,gfs=12::Int64,shape=:circle,cb=:lightrainbow)
+function plot_gradmap(xrange=(-3000,3000),yrange=(-3000,3000);autoscale=true::Bool,fn1="pxp-ini.ini"::String,fn2="residual_grad.out"::String,fno="gradmap.pdf"::String,plot_size=(600,500),lmargin=2.5,tmargin=1.0,bmargin=1.0,rmargin=1.0,show=false::Bool,ms1=10::Int64,ms2=3::Int64,gfs=12::Int64,shape=:circle,cb=:lightrainbow)
   a = DelimitedFiles.readdlm(fn1)
   numk = size(a)[1]
   px = a[1:numk,1]
